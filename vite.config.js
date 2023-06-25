a5853bc0496238187b295c799746a09d093b8e63
alvittoag <alvittogenz09@gmail.com>
2023-06-25 22:59:05 +0700

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,41 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "TripEase",
+        short_name: "Trip",
+        description: "Website pengelola travel booking",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#FFFFFF",
+        background_color: "#FFFFFF",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
       },
     }),
   ],
