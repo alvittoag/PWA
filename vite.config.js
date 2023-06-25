e92cf57d1fd305cdd208cd1eac33b2ce34d2b2dd
alvittoag <alvittogenz09@gmail.com>
2023-06-25 22:26:44 +0700

    add pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const pwaPlugin = createPWA({
+  config: require("./pwa.config.js"),
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pwaPlugin],
 });
